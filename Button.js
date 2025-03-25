a513e45efa2c6d98c854e3646c3cb8e60f522d7d
amkweb <athuyrain@gmail.com>
2025-03-25 13:51:32 +0630

    Update Button.js
    Signed-off-by: amkweb <athuyrain@gmail.com>

--- Button.js
+++ Button.js
@@ -1,4 +1,15 @@
-// JavaScript
-document.getElementById('improve-btn').addEventListener('click', function() {
-  alert('Thanks for helping improve the page!');
-});
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if(username && password) {
+        document.getElementById('message').innerText = "Welcome, " + username + "!";
+    } else {
+        document.getElementById('message').innerText = "Please enter username and password.";
+    }
+}
+
+function logout() {
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+    document.getElementById('message').innerText = "You have logged out.";
+}
